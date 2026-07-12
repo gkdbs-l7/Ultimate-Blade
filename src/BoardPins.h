b7384609fe781fff37354bf60c8007d5c935432e
neon <deokam2016@gmail.com>
2026-07-13 06:34:21 +0900

    esp32-c3 supermimi 세팅값 수정

--- src/BoardPins.h
+++ src/BoardPins.h
@@ -1,6 +1,25 @@
 #pragma once
 
 
+#ifdef USE_ESP32_C3
+
+#define OLED_SDA 8
+#define OLED_SCL 9
+
+#define BATTERY_PIN 0
+
+#define ENC_CLK 1
+#define ENC_DT  2
+#define ENC_SW  3
+
+#define LED_R 5
+#define LED_G 6
+#define LED_B 7
+
+#define VIBRATION_PIN 10;
+
+#else
+
 #define LED_R 25
 #define LED_G 32
 #define LED_B 33
@@ -13,3 +32,5 @@
 #define ENC_SW  18
 
 #define BATTERY_PIN 34
+
+#endif
